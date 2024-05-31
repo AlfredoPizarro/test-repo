3db20b8164c9f04a4fbdb873a856d14128e1856f
root <root@bastion.testlab.local>
2024-05-31 15:51:04 -0400

    First commit

---
+++ report/report.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const https = require('https');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8443;
+
+const options = {
+  key: fs.readFileSync('/etc/tls/tls.key'),
+  cert: fs.readFileSync('/etc/tls/tls.crt')
+};
+
+app.get('/report', (req, res) => {
+  res.send('Hello, World!');
+});
+
+https.createServer(options, app).listen(port, () => {
+  console.log(`Server is running on https://localhost:${port}/report`);
+});
